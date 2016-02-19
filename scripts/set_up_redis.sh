7754abaf33015366e13f6cf334202463016d42b7
Ashish Sonone <ashson07@gmail.com>
2016-02-19 15:30:35 +0530

    scripts : set up redis : install latest version, disable redis(service startup), and use supervisor only

--- scripts/set_up_redis.sh
+++ scripts/set_up_redis.sh
@@ -12,19 +12,33 @@
 #can pass arguments if like
 echo $1
 
-#install redis-server
+echo "installing redis-server from ppa:chris-lea"
+#install redis-server (download source and install)
 #apt-get : -y (assume yes), -q (quiet)
-apt-get -yq install redis-server
+sudo add-apt-repository ppa:chris-lea/redis-server -y
+sudo apt-get -q update
+sudo apt-get install -yq redis-server
 
+echo "stopping redis service and disable on startup"
+#disable start on boot
+sudo service redis-server stop
+sudo update-rc.d redis-server disable
+
+echo "creating directories"
 #create log directory
 LOG_DIR=/var/log/redis
+rm -r /var/log/redis
 mkdir -p $LOG_DIR
 chown -R mongodb:mongodb $LOG_DIR
+chmod -R 775 $LOG_DIR
 
 #create working directory
 WORK_DIR=/home/ashish/redis
 mkdir -p $WORK_DIR
+chown -R mongodb:mongodb $WORK_DIR
+chmod -R 775 $WORK_DIR
 
+echo "configuring supervisor"
 #create supervisor conf file for redis
 cat > /etc/supervisor/conf.d/redis.conf << EOF
 [program:redis]
@@ -42,4 +56,5 @@ EOF
 #update supervisor
 supervisorctl reread
 supervisorctl update
-supervisorctl status redis
+supervisorctl restart redis
+supervisorctl status
